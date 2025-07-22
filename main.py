8f7668bd997eb6d776277cd6111361e9a7505e21
Giancarlo Carvajal Flor <nitroso1814@gmail.com>
2025-07-22 12:12:46 -0500

    feat: add test cec endpoint

--- main.py
+++ main.py
@@ -25,3 +25,19 @@ async def create_lead(body: dict):
     parsed_body = await parser.parse_leadgen(body)
     lead_id = await odoo_api.create_lead(parsed_body)
     return {"Created lead id": lead_id}
+
+@app.get("/webhooks/cec/", status_code=200)
+async def subscribe(
+        hub_mode: str = Query(..., alias="hub.mode"),
+        hub_challenge: str = Query(..., alias="hub.challenge"),
+        hub_verify_token: str = Query(..., alias="hub.verify_token")
+):
+    if hub_mode == "subscribe" and hub_verify_token == META_VERIFY_TOKEN:
+        print("Webhook verified successfully!")
+        return Response(content=hub_challenge, media_type="text/plain")
+    else:
+        raise HTTPException(status_code=403, detail="Verification failed")
+
+@app.post("/webhooks/cec/", status_code=200)
+async def create_lead(body: dict):
+    print(body)
