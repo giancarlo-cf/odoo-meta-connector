a3d05f027f05e786c503776fcf40398c4d45aaad
Giancarlo Carvajal Flor <nitroso1814@gmail.com>
2025-09-10 09:20:04 -0500

    feat: add cec formdata api-key protected endpoint

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, Query, Response, HTTPException
+from fastapi import FastAPI, Query, Request, Response, HTTPException, Depends
+from fastapi.security import APIKeyHeader
 import os
 import parser
 import odoo_api
+import json
 
 app = FastAPI()
+header_scheme = APIKeyHeader(name="X-Api-Key")
 META_VERIFY_TOKEN = os.getenv("META_VERIFY_TOKEN", "invalid_token")
+API_KEY = os.getenv("API_KEY")
 
 
 @app.get("/webhooks/", status_code=200)
@@ -45,6 +49,17 @@ async def create_lead(body: dict):
     parsed_body = await parser.parse_leadgen(body)
     lead_id = await odoo_api.create_lead(parsed_body)
     return {"Created lead id": lead_id}
+
+
+@app.post("/cec/", status_code=200)
+async def create_lead(request: Request, key: str = Depends(header_scheme)):
+    if key != API_KEY:
+        raise HTTPException(status_code=403, detail="Invalid API key")
+
+    form_data = await request.form()
+    print(form_data)
+    print(json.dumps(dict(form_data), indent=4))
+    return {"status": "ok", "message": "Form data received and printed"}
 
 
 @app.get("/health/")
